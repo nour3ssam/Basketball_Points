1a1fc597bce0fb6ff85b0adce6d36912b3cc36ac
nour_essam <nouressam1123@gamil.com>
2024-10-25 16:31:18 +0300

    enhance UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,300 +1,222 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(CounterPoint());
+  runApp(const CounterPoint());
 }
 
 class CounterPoint extends StatefulWidget {
- 
+  const CounterPoint({super.key});
+
   @override
   State<CounterPoint> createState() => _CounterPointState();
 }
 
-    int PointA=0;
-    int PointB=0;
+ int PointA = 0;
+ int PointB = 0;
+
 class _CounterPointState extends State<CounterPoint> {
   @override
   Widget build(BuildContext context) {
-    // void nour(){
-    //   print("add one point");
-    // }
-    return MaterialApp (
+    return MaterialApp(
+     
       debugShowCheckedModeBanner: false,
-      home: Scaffold (
-      appBar: AppBar(
-        backgroundColor:Color(0xFFFFA500),
-        title: Text("Points Counter"),
-        ),
-        body:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text("Points Counter",
+            style: TextStyle(color:Colors.white,
+            fontSize: 28),
+             ),
+          ),
+          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
               children: [
-
-                Column(
-
-                  children: [
-
-                    Text(
-                       "Team A",
-                       style: TextStyle(
+                Column(children: [
+                  const Text("Team A",
+                      style:  TextStyle(
                         fontSize: 40,
-                        color: Color(0xFFFFA500),
-                        )
-                      ),
-
-
-                    Text(
-                      
-                       "$PointA",
-                       style: TextStyle(
+                         
+                      )),
+                  Text("$PointA",
+                      style: const TextStyle(
                         fontSize: 150,
-                        color: Color(0xFFFFA500),
-                        )
+                     
+                      )),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.blue),
+                        fixedSize: const WidgetStatePropertyAll(Size(165, 40))),
+                    onPressed: () {
+                      setState(() {
+                        PointA++;
+                      });
+                    },
+                    child: const Text(
+                      "Add 1 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-
-
-
-                        ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40))
-                       ),
-
-                        onPressed: () {
-                          setState(() {    
-                           PointA++;
-                          });
-                        },
-                        
-                       child: Text(
-                        "Add 1 point",
-                        style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                        ),
-                        ),
-
-                      
-                      ),
-
-                      SizedBox(
+                    ),
+                  ),
+                  const SizedBox(
                     height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                           WidgetStateProperty.all(Colors.blue),
+                      fixedSize: const WidgetStatePropertyAll(Size(165, 40)), // not change size of button
+                      //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
                     ),
-
-
-
-                      
-                      ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40)), // not change size of button 
-                       //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
-                       ),
-
-                        onPressed:(){
-                          setState(() {
-                          PointA+=2;
-                          });
-                        }, 
-                      
-                       child: Text(
-                        "Add 2 point",
-                         style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                         ),
+                    onPressed: () {
+                      setState(() {
+                        PointA += 2;
+                      });
+                    },
+                    child: const Text(
+                      "Add 2 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-
                     ),
-
-
-
-                    SizedBox(
+                  ),
+                  const SizedBox(
                     height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStateProperty.all(Colors.blue),
+                      fixedSize: const WidgetStatePropertyAll( Size(165, 40)), // not change size of button
+                      //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
                     ),
-
-
-
-
-                     ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40)), // not change size of button 
-                       //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
-                       ),
-
-                        onPressed:(){
-                          setState(() {
-                          PointA+=3;     
-                          });
-                        }, 
-                      
-                       child: Text(
-                        "Add 3 point",
-                         style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                        ),
+                    onPressed: () {
+                      setState(() {
+                        PointA += 3;
+                      });
+                    },
+                    child: const Text(
+                      "Add 3 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-
                     ),
- 
-
-                  ]
+                  ),
+                ]),
+                const SizedBox(
+                  height: 400,
+                  child: VerticalDivider(
+                    // you must use SizedBox
+                    color: Colors.black38,
+                    thickness: 1,
+                  ),
                 ),
-
-
-              SizedBox(
-                height: 400,
-                 child: VerticalDivider( // you must use SizedBox 
-                      color: Colors.black38,
-                      thickness: 1,
-                   ),
-              ),
-               
-
-
-
-
-                Column(
-                  children: [
-
-                    Text(
-                       "Team B",
-                       style: TextStyle(
+                Column(children: [
+                  const Text("Team B",
+                      style: TextStyle(
                         fontSize: 40,
-                        color: Color(0xFFFFA500),
-                        )
-                      ),
-
-
-                    Text(
-                       "$PointB",
-                       style: TextStyle(
+                      
+                      )),
+                  Text("$PointB",
+                      style: const TextStyle(
                         fontSize: 150,
-                        color: Color(0xFFFFA500),
-                        )
-                      ),
-
-
-
-                        ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40))
-                       ),
-
-                        onPressed: (){
-                          setState(() {
-                          PointB++;
-                          });
-                        },
-
-                       child: Text(
-                        "Add 1 point",
-                        style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                        ),
-                        ),
-
-                      ),
-
-                      SizedBox(
-                    height: 20,
-                    ),
-
-
-                      ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40)), // not change size of button 
-                       //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
-                       ),
-
-                        onPressed:(){
-                          setState(() {
-                          PointB+=2;
-                          });
-                        }, 
                       
-                       child: Text(
-                        "Add 2 point",
-                         style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                         ),
+                      )),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.blue),
+                        fixedSize: const WidgetStatePropertyAll(Size(165, 40))),
+                    onPressed: () {
+                      setState(() {
+                        PointB++;
+                      });
+                    },
+                    child: const Text(
+                      "Add 1 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-
                     ),
-
-
-                    SizedBox(
+                  ),
+                  const SizedBox(
                     height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                         WidgetStateProperty.all(Colors.blue),
+                      fixedSize: const WidgetStatePropertyAll(
+                          Size(165, 40)), // not change size of button
+                      //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
                     ),
-
-                     ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40)), // not change size of button 
-                       //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
-                       ),
-
-                        onPressed:(){
-                          setState(() {
-                          PointB+=3;
-                          });
-                        }, 
-                      
-                       child: Text(
-                        "Add 3 point",
-                         style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                         ),
+                    onPressed: () {
+                      setState(() {
+                        PointB += 2;
+                      });
+                    },
+                    child: const Text(
+                      "Add 2 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-
                     ),
-
-                  ]
-                ),
-
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                           WidgetStateProperty.all(Colors.blue),
+                      fixedSize: const WidgetStatePropertyAll(Size(165, 40)), // not change size of button
+                      //minimumSize: WidgetStatePropertyAll(Size(165, 40)),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        PointB += 3;
+                      });
+                    },
+                    child: const Text(
+                      "Add 3 point",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ]),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
-
-             ElevatedButton(
-                        style: ButtonStyle(
-                       backgroundColor: WidgetStateProperty.all(Color(0xFFFFA500)),
-                       fixedSize:WidgetStatePropertyAll(Size(165, 40))
-                       ),
-
-                        onPressed: () {
-                          setState(() {
-                            PointB=0;
-                            PointA=0;
-                          });
-                        },
-                        
-                       child: Text(
-                        "reset",
-                        style : TextStyle(
-                          fontSize: 22,
-                          color :Colors.white,
-                        ),
-                        ),
-                      
-                      ),
-          ]
-        )
-
-      ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:  WidgetStateProperty.all(Colors.blue),
+                  fixedSize: const WidgetStatePropertyAll(Size(165, 40))),
+              onPressed: () {
+                setState(() {
+                  PointB = 0;
+                  PointA = 0;
+                });
+              },
+              child: const Text(
+                "reset",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ])),
     );
   }
 }
-
